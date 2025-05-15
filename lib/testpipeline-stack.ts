c5deb008c87a3525d5232e848aa1820cc560b088
somenathghosh04 <somenath.ghosh04@gmail.com>
2025-05-15 09:01:01 -0500

    Added Dev stage

--- lib/testpipeline-stack.ts
+++ lib/testpipeline-stack.ts
@@ -25,8 +25,16 @@ export class TestpipelineStack extends Stack {
     // deployes to pipeline account
 
     const app = new testpipelinestage(this,"appdeploy");
-    const pipelinedeploystage = cdkpipeline.addStage(app);
+    const pipelinestage = cdkpipeline.addStage(app);
 
+    const devStage = new testpipelinestage(this, 'Dev', {
+      env: { 
+        account: '345990532018',  
+        region: 'us-east-1'
+      }
+    });
+    const adddevstage = cdkpipeline.addStage(devStage);
+    adddevstage.addPre(new ManualApprovalStep('ManualApprovalStep'));    
 
 }
 }
